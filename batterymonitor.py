45b5a0b79377a1cae82feb750570ec3e51e6a299
Fye <hereisnirvana@hotmail.com>
2022-03-13 12:48:27 -0600

    lectura y escritura desde registro

--- batterymonitor.py
+++ batterymonitor.py
@@ -1,27 +1,15 @@
 from os import path, system
-from re import sub, match
+from re import sub
 from sys import argv
 from time import sleep
 from urllib.request import urlopen, Request
+import winreg
 from psutil import sensors_battery
 from ruamel.yaml import YAML
 system('cls')
 
-#Set and write new current charging status
-def setCharging(newChargingStatus):
-    global charging
-    global config
-    charging = newChargingStatus
-    config['charging'] = charging
-    with open(configFile, 'w') as file:
-        yaml.dump(config, file)
-
-#Perform a simple GET request and return response data
-def GET(url):
-    try:
-        with urlopen(Request(url)) as response:
-            return (response.status, response.read().decode())
-    except: return None
+#Current charging status
+charging = False
 
 #Checks battery's current percent each 0.5 sec
 running = True
@@ -33,18 +21,18 @@ def start():
         battery = sensors_battery()
 
         if(battery.percent < minPercent): #If min percent reached...
-            setCharging(True)
+            setChargingStatus(True)
         elif(battery.percent > maxPercent): #If max percent reached...
-            setCharging(False)
+            setChargingStatus(False)
 
         if(charging and not battery.power_plugged): #If battery must be connected...
             connect()
         elif(not charging and battery.power_plugged): #If battery must be disconnected...                plugOff()
             disconnect()
         else:
-            print('ok')
+            print(charging, battery.percent)
         
-        sleep(0.5)
+        sleep(1.05)
 
 #When the battery must be connected...
 def connect():
@@ -56,28 +44,44 @@ def disconnect():
     print(GET(offURL))
     sleep(5)
 
+#Write current charging status into registry
+def setChargingStatus(newChargingStatus):
+    global charging
+    charging = newChargingStatus
+    with winreg.CreateKey(winreg.HKEY_CURRENT_USER, regKeyName) as newKey:
+        winreg.SetValueEx(newKey, regValueName, 0, winreg.REG_DWORD, charging)
+
+#Read current charging status from registry
+def loadChargingStatus():
+    global charging
+    try:
+        with winreg.OpenKey(winreg.HKEY_CURRENT_USER, regKeyName, 0, winreg.KEY_ALL_ACCESS) as key:
+            charging = winreg.QueryValueEx(key, regValueName)[0]
+    except:
+        setChargingStatus(charging)
+
+#Perform a GET request and return response data as a tuple
+def GET(url):
+    try:
+        with urlopen(Request(url)) as response:
+            return (response.status, response.read().decode())
+    except: return None
+
 #Getting current config as variables from the "config.yaml" file
 configFile = sub('\\\\[^\\\\]*$', '', path.realpath(__file__)) + '\config.yaml'
 yaml = YAML()
 with open(configFile) as file:
     config = yaml.load(file)
     locals().update(config)
+    loadChargingStatus()
 
-#Checking if there is a file as an argument, if so have it open when this script is closed
+#Checking if there is a file path as an argument, if so have it open when this script is closed
 caller = None
 callerPath = None
 if len(argv) > 1:
     if path.exists(argv[1]):
         caller = path.realpath(argv[1])
         callerPath = sub('\\\\[^\\\\]*$', '', caller)
-    print(caller)
-    print(callerPath)
-
-#If there's a "charging" status as argument, set it as current charging status
-if len(argv) > 2:
-    charging = True if match(r'(?i)^(?:1|true)$', argv[2]) else False
-    setCharging(charging)
-    print(argv[2])
 
 #Start battery monitor
 start()
