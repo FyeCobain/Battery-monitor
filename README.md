1675a018307abc07d1d07a8f543bf745a4871f02
Fye <hereisnirvana@hotmail.com>
2022-03-15 22:07:04 -0600

    Beta 0.2.0

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Battery Monitor
 
-_**Version** 0.1.0 (beta)_
+_**Version** 0.2.0 (beta)_
 
-A simple python script for Windows to monitor laptop battery percentage and prevent it from being too low or too high, helping to extend battery life.
+A simple python script for **Windows** to monitor laptop battery percentage and prevent it from being too low or too high, helping to extend battery life.
 
 ### Requirements
 > **psutil**
@@ -18,8 +18,8 @@ pip install ruamel.yaml
 
 - You can configure two URLs so that the script makes a GET request when the percentage is less than the minimum or greater than the maximum set.
 Designed to set up an IFTTT webhook or Tuya link to turn on/off a smart plug.
-If no URLs are set, the script will display an alert to the user.
-- You can open it and set as argument the path to a file, when closing the script, the file will be opened. Thought to open the script from an application that will be closed to avoid conflicts and will be opened again automatically.
+If no URLs are set, the script will play an alert sound.
+- You can open it and set as argument the path to a file, when closing the script, the file will be opened. Thought to open the script from an application that will be closed to avoid conflicts and must be reopened again.
 ```console
 batterymonitor.py "C:\scripts\OpenWhenClosing.exe"
 ```
--- batterymonitor.py
+++ batterymonitor.py
@@ -1,64 +1,76 @@
 from os import path, system
 from re import sub
+import subprocess, shlex
 from sys import argv
-from time import sleep
+from time import sleep, time
+from tkinter import Tk
 from urllib.request import urlopen, Request
 import winreg
+from winsound import Beep
 from psutil import sensors_battery
 from ruamel.yaml import YAML
 
-#Current charging status
+# Current charging status
 charging = False
 
-#Checks battery's current percent each 0.5 sec
+# Checks battery's current percent each 0.5 sec
 running = True
 def start():
     global running
     global charging
     while(running):
-        #Getting battery info
+        # Getting battery info
         battery = sensors_battery()
 
-        if(battery.percent < minPercent): #If min percent reached...
+        if(battery.percent <= minPercent): # If min percent reached...
             setChargingStatus(True)
-        elif(battery.percent > maxPercent): #If max percent reached...
+        elif(battery.percent >= maxPercent): # If max percent reached...
             setChargingStatus(False)
 
-        if(charging and not battery.power_plugged): #If battery must be connected...
-            connect()
+        if(charging and not battery.power_plugged): # If battery must be connected...
+            connect(battery.percent)
         elif(not charging and battery.power_plugged): #If battery must be disconnected...                plugOff()
-            disconnect()
-        else:
-            print(charging, battery.percent)
+            disconnect(battery.percent)
         
         sleep(1.05)
 
-#When the battery must be connected...
-def connect():
+# When the battery must be connected...
+def connect(batteryPercent):
+    # Triying to make GET request
     if(onURL):
-        print(GET(onURL))
-        sleep(5)
-    else:
-        print('Please connect your battery...')
-        sleep(10)
+        response = GET(onURL)
+        if(response):
+            if(response[0] == 200):
+                sleep(5)
+                return
 
-#When the battery must be disconnected...
-def disconnect():
+    # Play a sound for "low battery"
+    Beep(655, 250)
+    Beep(655, 300)
+    sleep(5)
+
+# When the battery must be disconnected...
+def disconnect(batteryPercent):
+    # Triying to make GET request
     if(offURL):
-        print(GET(offURL))
-        sleep(5)
-    else:
-        print('Please disconnect your battery...')
-        sleep(10)
+        response = GET(offURL)
+        if(response):
+            if(response[0] == 200):
+                sleep(5)
+                return
 
-#Write current charging status into registry
+    # Play a sound for "battery too charged"
+    Beep(655, 550)
+    sleep(5)
+
+# Write current charging status into registry
 def setChargingStatus(newChargingStatus):
     global charging
     charging = newChargingStatus
     with winreg.CreateKey(winreg.HKEY_CURRENT_USER, regSubKeyName) as newKey:
         winreg.SetValueEx(newKey, regValueName, 0, winreg.REG_DWORD, charging)
 
-#Read current charging status from registry
+# Read current charging status from registry
 def loadChargingStatus():
     global charging
     try:
@@ -67,14 +79,14 @@ def loadChargingStatus():
     except:
         setChargingStatus(charging)
 
-#Perform a GET request and return response data as a tuple
+# Perform a GET request and return response data as a tuple
 def GET(url):
     try:
         with urlopen(Request(url)) as response:
             return (response.status, response.read().decode())
     except: return None
 
-#Getting current config as variables from the "config.yaml" file
+# Getting current config as variables from the "config.yaml" file
 configFile = sub('\\\\[^\\\\]*$', '', path.realpath(__file__)) + '\config.yaml'
 yaml = YAML()
 with open(configFile) as file:
@@ -82,7 +94,7 @@ with open(configFile) as file:
     locals().update(config)
     loadChargingStatus()
 
-#Checking if there is a file path as an argument, if so have it open when this script is closed
+# Checking if there is a file path as an argument, if so have it open when this script is closed
 caller = None
 callerPath = None
 if len(argv) > 1:
@@ -90,5 +102,5 @@ if len(argv) > 1:
         caller = path.realpath(argv[1])
         callerPath = sub('\\\\[^\\\\]*$', '', caller)
 
-#Start battery monitor
+# Start battery monitor
 start()
